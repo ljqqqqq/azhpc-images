8fe882f8425d7abe148b7527b38cdaa99781415f
Jiaqi Li <ljiaqi@microsoft.com>
2025-09-26 00:49:06 -0700

    fix lustre version

--- ubuntu/common/setup_lustre_repo.sh
+++ ubuntu/common/setup_lustre_repo.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -ex
 
-source /etc/lsb-release    
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
-curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+source /etc/lsb-release
+if [[ "$ARCH" == "aarch64" ]]; then
+    echo "deb [arch=arm64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
+else
+    echo "deb [arch=amd64] https://packages.microsoft.com/repos/amlfs-${DISTRIB_CODENAME}/ ${DISTRIB_CODENAME} main" | tee /etc/apt/sources.list.d/amlfs.list
+fi
+    curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
+
