f81cc29be4b940de8e3bdd8fe6629dbff38b01e9
Jiaqi Li <ljiaqi@microsoft.com>
2025-10-02 02:09:31 -0700

    remove topology file

--- customizations/ndv6.sh
+++ customizations/ndv6.sh
@@ -4,15 +4,5 @@ set -ex
 # Place NDv6 customizations under /opt/microsoft/ndv6
 mkdir -p /opt/microsoft/ndv6
 
-# Link the NDv6 topology file into /opt/microsoft/ndv6/
-ln -sf /opt/microsoft/ndv6-topo.xml /opt/microsoft/ndv6/topo.xml
-
-## Set NCCL configuration file for NDv6
-bash -c "cat > /etc/nccl.conf" <<'EOF'
-NCCL_IB_PCI_RELAXED_ORDERING=1
-NCCL_TOPO_FILE=/opt/microsoft/ndv6/topo.xml
-NCCL_IGNORE_CPU_AFFINITY=1
-EOF
-
 ## load nvidia-peermem module
 modprobe nvidia-peermem
