1ab281994dc6bb718f48e2d14af9aff1abb96d11
Jiaqi Li <ljiaqi@microsoft.com>
2025-10-08 19:15:11 -0700

    update nvidia container toolkit

--- ubuntu/common/install_nvidia_container_toolkit.sh
+++ ubuntu/common/install_nvidia_container_toolkit.sh
@@ -5,15 +5,19 @@ set -ex
 # Reference: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html
 # Setting up NVIDIA Container Toolkit
 curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/deb/nvidia-container-toolkit.list | \
     sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
     sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
-sed -i -e '/experimental/ s/^#//g' /etc/apt/sources.list.d/nvidia-container-toolkit.list
+nvidia_toolkit_version="1.18.0~rc.5-1"
 
 apt-get update
 # Install NVIDIA container toolkit and mark NVIDIA packages on hold
-apt-get install -y nvidia-container-toolkit
+sudo apt-get install -y libnvidia-container1=$nvidia_toolkit_version
+sudo apt-get install -y libnvidia-container-tools=$nvidia_toolkit_version
+sudo apt-get install -y nvidia-container-toolkit-base=$nvidia_toolkit_version
+sudo apt-get install -y nvidia-container-toolkit=$nvidia_toolkit_version
+
 apt-mark hold nvidia-container-toolkit
 apt-mark hold libnvidia-container-tools
 apt-mark hold libnvidia-container1
