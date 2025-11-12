78da482173e3ad8c174afda8cd7997c4d799284d
Jiaqi Li <ljiaqi@microsoft.com>
2025-11-12 01:26:47 -0800

    upgrade versions

--- ubuntu/ubuntu-24.x/ubuntu-24.04-hpc/gb200/install_nvshmem.sh
+++ ubuntu/ubuntu-24.x/ubuntu-24.04-hpc/gb200/install_nvshmem.sh
@@ -1,6 +1,6 @@
 set -ex
 
-apt install libnvshmem3-cuda-12 libnvshmem3-dev-cuda-12
-nvshmem_version=$(apt list --installed | grep libnvshmem3-cuda-12/ | cut -d' ' -f2)
+apt install libnvshmem3-cuda-13 libnvshmem3-dev-cuda-13
+nvshmem_version=$(apt list --installed | grep libnvshmem3-cuda-13   / | cut -d' ' -f2)
 
 $COMMON_DIR/write_component_version.sh "NVSHMEM" $nvshmem_version
